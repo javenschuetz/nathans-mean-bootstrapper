0259e8c1c2d9b24b273fbd2402a188db40b0cb15
jammygit <mail@nathanschuetz.ca>
2018-12-26 00:08:57 -0700

    refactor: rearrange directories and separate routes

---
+++ accounts/account_routes.js
@@ -0,0 +1,103 @@
+'use strict';
+
+// libraries
+const bcrypt = require('bcryptjs'); // cryptographic hashing algorithm for passwords
+const csurf = require('csurf'); // mitigates against cross site request forgeries
+const express = require('express');
+
+// project modules
+const User = require('./User');
+
+// config
+const router = express.Router();
+
+
+
+// ********************************************************************** config
+router.use(csurf());
+
+
+
+// ********************************************************************** routes
+router.get('/login', (req, res) => {
+    res.render('login', {csrfToken: req.csrfToken()});
+});
+
+// todo - rate limiting in nginx for this
+router.post('/login', (req, res) => {
+    User.findOne({email: req.body.email}, (err, user) => {
+        if (err || !user || !bcrypt.compareSync(req.body.password, user.password)) {
+            return res.render('login', {csrfToken: req.csrfToken()});
+        }
+
+        // if logged in, use client-side session to stay logged in
+        req.session.user_id = user._id;
+        return res.redirect('/dashboard');
+    });
+});
+
+router.get('/register', (req, res) => {
+    res.render('register', {csrfToken: req.csrfToken()});
+});
+
+// OWASP guidelines: https://www.owasp.org/index.php/Authentication_Cheat_Sheet
+function check_password_strength(email, password) {
+
+    const length = password.length;
+    if (length < 10) return { strong: false, reason: 'password is too short'};
+    if (length > 128) return { strong: false, reason: 'password is too long'};
+    if (password.includes(email)) return { strong: false, reason: 'password cannot contain that'};
+    if (password.includes('password')) return { strong: false, reason: 'password cannot contain that'};
+    if (password.includes('Password')) return { strong: false, reason: 'password cannot contain that'};
+
+    let score = 0;
+    if (length >= 12) score++;
+    if (length >= 16) score++;
+    if (password.match(/\d+/)) score++; // 1+ numbers
+    if (password.match(/[a-z]/)) score++; // lowercase
+    if (password.match(/[A-Z]/)) score++; // uppercase
+    if (password.match(/.[!,@,#,$,%,^,&,*,?,_,~,-,£,(,)]/)) score++; // special characters
+
+    return {
+        strong: score >= 3,
+        reason: `unless a password is quite long, it must satisfy 3/4 of the following criteria:
+    - contain a number
+    - contain a lowercase letter
+    - contain an uppercase letter
+    - contain a special character`
+    };
+}
+
+router.post('/register', (req, res) => {
+    const password_strength = check_password_strength(req.body.email, req.body.password);
+    if (!password_strength.strong) {
+        // todo - use the nice error messages in the rendering so the user can see them
+        return res.render('register', {csrfToken: req.csrfToken()});
+    }
+
+    // make sure to replace pw with hashed password before storing the user
+    const hash = bcrypt.hashSync(req.body.password, parseInt(process.env.WORK_FACTOR));
+    req.body.password = hash;
+    let user = new User(req.body);
+
+    user.save((err) => {
+        if (err) {
+            // todo - make use of this (flash? template variable?)
+            let error = "error when saving this user";
+
+            if (err.code === 11000) {
+                error = "email taken";
+            }
+            // not a PRG, but should be fine
+            return res.render('register', {csrfToken: req.csrfToken()});
+        }
+        return res.redirect('login');
+    });
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie("session");
+    res.redirect('/');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 'use strict';
 
 // libraries (alphabetic)
-const bcrypt = require('bcryptjs');                 // cryptographic hashing algorithm for passwords
 const body_parser = require('body-parser');         // to give us access to body of http requests
 const client_sessions = require('client-sessions'); // mozilla library to implement encrypted client-side sessions
-const csurf = require('csurf');                     // mitigates against cross site request forgeries
 const express = require('express');
 const helmet = require('helmet');                   // add some nice security headers
 const mongoose = require('mongoose');               // mongo schemas
-const path = require('path');                       // helps resolve file paths
 
 // project modules
-const User = require('./models/User');              // User Schema
+const User = require('./accounts/User');              // User Schema
 
 
 
@@ -19,8 +16,8 @@ const User = require('./models/User');              // User Schema
 const app = express();
 mongoose.connect('mongodb://localhost/backbone-db');        // maintains a connection to mongo
 app.set('view engine', 'ejs');                              // says 'we're using ejs'
-app.set('views', path.join(`${__dirname}/views`));          // says 'our ejs is here'
-app.use('/static', express.static(`${__dirname}/static`));  // sort of automatic route handling for this directory
+app.set('views', `${__dirname}/client/views`);          // says 'our ejs is here'
+app.use('/static', express.static(`${__dirname}/client/static`));  // sort of automatic route handling for this directory
 
 
 
@@ -63,8 +60,6 @@ app.use((req, res, next) => {
     });
 });
 
-app.use(csurf());
-
 // confirms that login was verified above
 function login_required(req, res, next) {
     if (!req.user) {
@@ -76,100 +71,15 @@ function login_required(req, res, next) {
 
 
 
-// ************************************************************ register & login
-// OWASP guidelines: https://www.owasp.org/index.php/Authentication_Cheat_Sheet
-function check_password_strength(email, password) {
+// ********************************************************************** routes
+// login, register, logout
+app.use('/accounts', require('./accounts/account_routes'));
 
-    const length = password.length;
-    if (length < 10) return { strong: false, reason: 'password is too short'};
-    if (length > 128) return { strong: false, reason: 'password is too long'};
-    if (password.includes(email)) return { strong: false, reason: 'password cannot contain that'};
-    if (password.includes('password')) return { strong: false, reason: 'password cannot contain that'};
-    if (password.includes('Password')) return { strong: false, reason: 'password cannot contain that'};
-
-    let score = 0;
-    if (length >= 12) score++;
-    if (length >= 16) score++;
-    if (password.match(/\d+/)) score++; // 1+ numbers
-    if (password.match(/[a-z]/)) score++; // lowercase
-    if (password.match(/[A-Z]/)) score++; // uppercase
-    if (password.match(/.[!,@,#,$,%,^,&,*,?,_,~,-,£,(,)]/)) score++; // special characters
-
-    return {
-        strong: score >= 3,
-        reason: `unless a password is quite long, it must satisfy 3/4 of the following criteria:
-    - contain a number
-    - contain a lowercase letter
-    - contain an uppercase letter
-    - contain a special character`
-    };
-}
-
-
-
-app.get('/register', (req, res) => {
-    res.render('register', {csrfToken: req.csrfToken()});
-});
-
-app.post('/register', (req, res) => {
-    const password_strength = check_password_strength(req.body.email, req.body.password);
-    if (!password_strength.strong) {
-        // todo - use the nice error messages in the rendering so the user can see them
-        return res.render('register', {csrfToken: req.csrfToken()});
-    }
-
-    // make sure to replace pw with hashed password before storing the user
-    const hash = bcrypt.hashSync(req.body.password, parseInt(process.env.WORK_FACTOR));
-    req.body.password = hash;
-    let user = new User(req.body);
-
-    user.save((err) => {
-        if (err) {
-            // todo - make use of this (flash? template variable?)
-            let error = "error when saving this user";
-
-            if (err.code === 11000) {
-                error = "email taken";
-            }
-            // not a PRG, but should be fine
-            return res.render('register', {csrfToken: req.csrfToken()});
-        }
-        return res.redirect('login');
-    });
-});
-
-app.get('/login', (req, res) => {
-    res.render('login', {csrfToken: req.csrfToken()});
-});
-
-// todo - rate limiting in nginx for this
-app.post('/login', (req, res) => {
-    User.findOne({email: req.body.email}, (err, user) => {
-        if (err || !user || !bcrypt.compareSync(req.body.password, user.password)) {
-            return res.render('login', {csrfToken: req.csrfToken()});
-        }
-
-        // if logged in, use client-side session to stay logged in
-        req.session.user_id = user._id;
-        return res.redirect('dashboard');
-    });
-});
-
-
-
-// ****************************************************** web app route handlers
+// main app dashboard
 app.get('/dashboard', login_required, (req, res) => {
     res.render('dashboard');
 });
 
-app.get('/logout', (req, res) => {
-    res.clearCookie("session");
-    res.redirect('/');
-});
-
-
-
-// ********************************************** unauthenticated route handlers
 // Default landing page is our informational site
 app.get('/', (req, res) => {
     res.render('index');
@@ -180,7 +90,9 @@ app.get('*', (req, res) => {
     res.redirect('/');
 });
 
-// error handling
+
+
+// ************************************************************** error handling
 app.use((err, req, res, next) => {
   res.status(500).send("Something broke :( Please try again.");
 });
