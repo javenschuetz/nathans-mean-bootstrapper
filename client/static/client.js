d8cde3bdc605fbf36fc7346cd052614b16201619
jammygit <mail@nathanschuetz.ca>
2018-12-29 03:08:04 -0700

    feat: generate graph via REST GET to server posture records

--- client/static/client.js
+++ client/static/client.js
@@ -15,35 +15,38 @@ angular.module('backbone_app', ['ngRoute', 'chart.js'])
 .controller('sanity_check_ctrl', ['$scope', function ($scope) {
     $scope.loaded = true;
 }])
-.controller("line_controller", ['$scope', function ($scope) {
+.controller("line_controller", ['$scope', '$http', function ($scope, $http) {
     $scope.foo = 'working';
 
-    const hours_per_day = 24;
-    const upright_minutes = new Array(hours_per_day).fill(null).map(_=> Math.floor(Math.random() * 60));
+    $http.get("https://posturetracking.com/posture_records/4edd40c86762e0da120a0003/1/1/1")
+    .then((response) => {
+        const minutes_per_hour = response.data[0].minutes_per_hour;
 
-    $scope.labels = Array.from(Array(hours_per_day).keys()).map( (n) => {
-        if (n === 0) return '12 am';
-        else if (n < 12) return `${n} am`;
-        else if (n === 12) return '12 pm';
-        return `${n-12} pm`;
-    });
-    $scope.series = ['Upright Minutses'];
-    $scope.data = [ upright_minutes ];
+        $scope.labels = Array.from(Array(24).keys()).map( (n) => {
+            if (n === 0) return '12 am';
+            else if (n < 12) return `${n} am`;
+            else if (n === 12) return '12 pm';
+            return `${n-12} pm`;
+        });
+        $scope.series = ['Upright Minutes'];
+        $scope.data = [ minutes_per_hour ];
 
-    $scope.onClick = function (points, evt) {
-        console.log(points, evt);
-    };
-    $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
-    $scope.options = {
-        scales: {
-            yAxes: [
-            {
-                id: 'y-axis-1',
-                type: 'linear',
-                display: true,
-                position: 'left'
+        $scope.onClick = function (points, evt) {
+            console.log(points, evt);
+        };
+        $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
+        $scope.options = {
+            scales: {
+                yAxes: [
+                    {
+                        id: 'y-axis-1',
+                        type: 'linear',
+                        display: true,
+                        position: 'left'
+                    }
+                ]
             }
-        ]
-    }
-  };
+        }
+    })
+    .catch(err => console.error(err));
 }]);
