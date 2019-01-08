0315f7a8a157cfc86066361231857b7b5a86447f
jammygit <mail@nathanschuetz.ca>
2019-01-08 13:03:09 -0700

    fix: Nodemon not restarting in vagrant w/o a legacy flag

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,4 @@ secret=$(head -c20 /dev/urandom | base64)
 export IS_DEV=1
 export BACKBONE_SECRET=${secret}
 
-node_modules/nodemon/bin/nodemon.js server.js
+node_modules/nodemon/bin/nodemon.js -L server.js
